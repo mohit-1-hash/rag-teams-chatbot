470af79751a37ca199bd26e8dbd0aad07a93d1ba
Mohit-1-hash <mohitvaranasi2006@gmail.com>
2026-07-14 23:41:04 +0530

    fixing small mistakes

--- llm/answer.py
+++ llm/answer.py
@@ -5,7 +5,7 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-genai.configure(api_key=os.getenv["GEMINI_API_KEY"])
+genai.configure(api_key=os.getenv("GEMINI_API_KEY"))
 llm = genai.GenerativeModel("gemini-2.5-flash-lite")
 
 SYSTEM_PROMPT = """
